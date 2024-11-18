68fa566381331343fb02db42056e890ff9513ebb
thisouza01 <tihsouza01@gmail.com>
2024-11-18 18:54:40 -0300

    concertando terceiro exercicio

--- aula1.py
+++ aula1.py
@@ -27,4 +27,4 @@ print('===============================')
 # terceiro: ler dois numeros e mostrar a soma entre eles
 num1 = input('Digite um numero: ')
 num2 = input('Digite outro numero: ')
-print(num1 + num2)
+print(int(num1) + int(num2))
