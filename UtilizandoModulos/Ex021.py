8a843a1f294f9820229ec64433d8a3237bc4f4f8
Thiago Souza <124224124+thisouza01@users.noreply.github.com>
2024-11-23 09:06:12 -0300

    Update Ex021.py

--- UtilizandoModulos/Ex021.py
+++ UtilizandoModulos/Ex021.py
@@ -1,6 +1,9 @@
 #ler um num de 0 a 9999 e mostrar cada digito Ex: 1234 4unidade, 3dezena, 2centena, 1milhar string e matematicamente
-numString = str(input('Digite um numero entre 0 e 9999: '))
+numInteiro = int(input('Digite um numero entre 0 e 9999: '))
 
+unidade = numInteiro // 1 % 10
+dezena = numInteiro // 10 % 10
+centena = numInteiro // 100 % 10
+milhar = numInteiro // 1000 % 10
 
-
-print('Milhar: {} \n Centena: {} \n Dezena: {} \n Unidade: {}'.format())    
+print('Milhar: {} \n Centena: {} \n Dezena: {} \n Unidade: {}'.format(unidade, dezena, centena, milhar))    
