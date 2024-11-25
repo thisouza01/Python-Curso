572235511233ad8b5370925112c7ddea3c90ed47
unknown <tihsouza01@gmail.com>
2024-11-25 16:32:06 -0300

    exercicio imc

--- Condicoes/Ex041.py
+++ Condicoes/Ex041.py
@@ -4,3 +4,21 @@
 # >= 25 and < 30 sobrepeso
 # >= 30 and < 40 Obesidade
 # >= 40 obesidade morbida
+
+peso = float(input('Digite seu peso: '))
+altura = float(input('Digite sua altura: '))
+
+# calcula IMC
+imc = peso / (altura ** 2)
+
+# verifica status IMC
+if imc < 18.5:
+    print('Abaixo do peso')
+elif imc >= 18.5 and imc < 25:
+    print('Peso ideal')
+elif imc >= 25 and imc < 30:
+    print('Sobrepeso')
+elif imc >= 30 and imc < 40:
+    print('Obesidade')
+elif imc >= 40:
+    print('Obesidade mórbida')                
