2760b5ff3090d8ca185e9272317df8f43086be93
unknown <tihsouza01@gmail.com>
2024-11-25 14:05:08 -0300

    triangulo v.02

--- Condicoes/Ex040.py
+++ Condicoes/Ex040.py
@@ -2,3 +2,18 @@
 # equilatero = todos lados iguais
 # isoceles = dois lados iguais
 # escaleno = todos os lados diferentes
+
+reta1 = int(input('Digite o tamanho da primeira reta: '))
+reta2 = int(input('Digite o tamanho da segunda reta: '))
+reta3 = int(input('Digite o tamanho da terceira reta: '))
+
+if (reta1 + reta2 > reta3) and (reta2 + reta3 > reta1) and (reta1 + reta3 > reta2):
+    print('A soma de dois lados é maior que o terceiro lado! É um triangulo')
+    if reta1 == reta2 and reta2 == reta3:
+        print('Triangulo equilatero')
+    elif (reta1 == reta2 and reta2 != reta3) or (reta3 == reta2 and reta1 != reta2) or (reta3 == reta1 and reta2 != reta3):
+        print('Triangulo isoceles')
+    elif (reta1 != reta2 and reta1 != reta3 and reta2 != reta3):
+        print('Triangulo escaleno')        
+else:
+    print('A soma de dois lados não é maio que o terceiro lado. Não é um triangulo')    
