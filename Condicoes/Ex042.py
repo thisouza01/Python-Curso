4c8acd34e0a8721c0637328f2e2a9f7da28ad0bc
unknown <tihsouza01@gmail.com>
2024-11-26 08:57:15 -0300

    exercicios condições

--- Condicoes/Ex042.py
+++ Condicoes/Ex042.py
@@ -4,3 +4,25 @@
 # à vista cartao = 0.05
 # ate 2x cartao = precoNoraml
 # 3x ou mais = + 0.20
+
+precoNormal = float(input('Digite o preço: '))
+
+# formas de pagamento
+print('Dinheiro, Cheque, Cartão 1x, 2x, 3x(com juros)')
+condPagamento = str(input('Forma de pagamento? '))
+
+
+if condPagamento.lower() == 'dinheiro' or condPagamento.lower() == 'cheque':
+    # calcula desconto
+    valorPago = precoNormal - (precoNormal * 0.10)
+    print('O valor com desconto é de {} reais'.format(valorPago))
+elif condPagamento.lower() == 'cartão 1x':
+    # calcula desconto
+    valorPago = precoNormal - (precoNormal * 0.05)
+    print('O valor com desconto é de {} reais'.format(valorPago))
+elif condPagamento.lower() == 'cartão 2x':
+    print('Não temos desconto, valor normal: {} reais'.format(precoNormal)) 
+elif condPagamento == 'cartão 3x':
+    # calcula juros
+    valorPago = precoNormal + (precoNormal * 0.20)
+    print('O valor com juros é de {} reais'.format(valorPago))       
--- Condicoes/Ex043.py
+++ Condicoes/Ex043.py
@@ -1,1 +1,33 @@
 # jokenpo
+from random import randint
+
+# escolha do computador
+numAleatorio = randint(1, 3)
+
+if numAleatorio == 1:
+    escolhaComp = 'Pedra'
+elif numAleatorio == 2:
+    escolhaComp = 'Papel'
+elif numAleatorio == 3:
+    escolhaComp = 'Tesoura'
+
+# escolha do usuário
+print('Escolha uma opção')
+escolhaUser = str(input('Pedra \nPapel \nTesoura \n'))  
+print()
+          
+# partida
+if escolhaComp == escolhaUser:
+    print('{} x {} : Empate!'.format(escolhaComp, escolhaUser))
+elif escolhaComp == 'Pedra' and escolhaUser == 'Papel':
+    print('{} x {} : Usuário ganhou!'.format(escolhaComp, escolhaUser))
+elif escolhaComp == 'Pedra' and escolhaUser == 'Tesoura':
+    print('{} x {} : Computador ganhou!'.format(escolhaComp, escolhaUser))
+elif escolhaComp == 'Papel' and escolhaUser == 'Pedra':
+    print('{} x {} : Computador ganhou!'.format(escolhaComp, escolhaUser))
+elif escolhaComp == 'Papel' and escolhaUser == 'Tesoura':
+    print('{} x {} : Usuário ganhou!'.format(escolhaComp, escolhaUser))
+elif escolhaComp == 'Tesoura' and escolhaUser == 'Pedra':        
+    print('{} x {} : Usuário ganhou!'.format(escolhaComp, escolhaUser))
+elif escolhaComp == 'Tesoura' and escolhaUser == 'Papel':
+    print('{} x {} : Computador ganhou!'.format(escolhaComp, escolhaUser))
