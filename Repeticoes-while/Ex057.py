45b96bf5392d3d31adb35195a7614fa396219075
unknown <tihsouza01@gmail.com>
2024-11-28 11:40:05 -0300

    preciso terminar

---
+++ Repeticoes-while/Ex057.py
@@ -0,0 +1,33 @@
+# recebe valores
+num1 = float(input('Digite um valor: '))
+num2 = float(input('Digite outro valor: '))
+
+# mostra opções
+print()
+print('1 - SOMAR \n2 - MULTIPLICAR \n3 - MAIOR \n4- NOVOS NUMEROS \n5- SAIR')
+print()
+
+# escolhas
+escolha = int(input('Escolha uma opção: '))
+
+# verificação
+while escolha != 5:
+    if escolha ==1:
+        #soma numeros
+        soma = num1 + num2
+        print(soma)
+    elif escolha == 2:
+        # multiplica numeros    
+        multiplica = num1 * num2
+        print(multiplica)
+    elif escolha == 3:
+        # maior numero
+        maior = max(num1, num2)    
+        print(maior)
+    elif escolha == 4:
+        print('Digite outro numero para adicionar')
+        novoNum = float(input())
+        print(novoNum)
+    else:
+        print('Digite um valor possível!')     
+
