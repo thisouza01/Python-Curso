9b2087acc9e1454d7b584be1f21115bf60f28355
thisouza01 <tihsouza01@gmail.com>
2024-12-05 20:16:23 -0300

    terminado exercicios

--- Tuplas/Ex075.py
+++ Tuplas/Ex075.py
@@ -1,8 +1,11 @@
-tupla = ('abacaxi', 'maca', 'pera', 'limao', 'caqui')
+tupla = ('abacaxi', 'maca', 'pera', 'limao', 'caqui', 'banana')
+
+lista_letra = []
 
 for item in tupla:
     print()
     for letra in item:
         if letra == 'a' or letra == 'e' or letra == 'i' or letra == 'o' or letra == 'u':
-
-            print(f'A palavra {item} tem as vogais {letra}')
+            lista_letra.append(letra)
+    print(f'A palavra {item} tem as vogais {lista_letra}')
+    lista_letra = []
