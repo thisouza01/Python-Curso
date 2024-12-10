ce9e4cd070613b1d4b4f3daf741837958e6159c6
unknown <tihsouza01@gmail.com>
2024-12-10 14:50:16 -0300

    Outra maneira realizada

--- Listas/Ex084b.py
+++ Listas/Ex084b.py
@@ -1,3 +1,22 @@
+# inicia listas
 matriz = []
-numeros = []
+linha = []
 
+# numero de linhas e colunas
+num_linhas = 3
+num_colunas = 3
+
+while len(matriz) != num_linhas:
+    # recebe numero
+    numero = int(input('Numero: '))
+
+    # adicion numero na linha
+    linha.append(numero)
+
+    if len(linha) == num_colunas:
+        # adiciona a linha a matriz
+        matriz.append(linha)
+        # esvazia a linha
+        linha = []
+
+print(matriz)        
