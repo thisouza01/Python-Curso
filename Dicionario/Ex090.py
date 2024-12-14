2e9e8cb204b5aec78282bd56159dbc2bc9d9d5ef
unknown <tihsouza01@gmail.com>
2024-12-14 14:49:08 -0300

    Exercicioquase pronto

--- Dicionario/Ex090.py
+++ Dicionario/Ex090.py
@@ -1,11 +1,17 @@
-
+# recebe dados pessoais
 dado_pessoal = {
     "nome": str(input('Nome: ')),
     "ano_nascimeto": int(input('Ano nascimento: ')),
     "CTPS": int(input('Carteira de Trabalho: (0 caso nao tenha) '))
 }
 
+# caso tenha CTPS, pergunte o ano que foi contratado e o salario
 if dado_pessoal['CTPS'] == 0:
     print(dado_pessoal)
 else:
-    dado_pessoal += "ano_contratacao": int(input('Ano de contratação: '))     
+    dado_pessoal.update({
+        'data_contratacao': int(input('Ano contratação: ')),
+        'salario': float(input('Salario: '))
+    })
+    print(dado_pessoal) 
+
