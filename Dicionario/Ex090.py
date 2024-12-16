1bd31d7234ed4c8d3d9cea8c3108959413fc6f32
unknown <tihsouza01@gmail.com>
2024-12-16 13:04:24 -0300

    melhando exercicios

--- Dicionario/Ex090.py
+++ Dicionario/Ex090.py
@@ -1,9 +1,19 @@
+from datetime import date
+
 # recebe dados pessoais
 dado_pessoal = {
     "nome": str(input('Nome: ')),
     "ano_nascimeto": int(input('Ano nascimento: ')),
     "CTPS": int(input('Carteira de Trabalho: (0 caso nao tenha) '))
 }
+
+# pega ano atual
+ano_atual = date.today().year
+
+# calcular idade
+idade = ano_atual - dado_pessoal['ano_nascimeto']
+print(idade)
+print()
 
 # caso tenha CTPS, pergunte o ano que foi contratado e o salario
 if dado_pessoal['CTPS'] == 0:
