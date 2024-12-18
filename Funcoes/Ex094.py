0c73d36f4ae222084d5b7f555a243524d3ad2e4b
unknown <tihsouza01@gmail.com>
2024-12-18 09:11:27 -0300

    exercicio funçoes

---
+++ Funcoes/Ex094.py
@@ -0,0 +1,9 @@
+# define função para calcular área
+def area(largura, altura):
+    area = largura * altura
+    return area
+
+# mostra as áreas
+print(f'A area é de {area(2, 10)}')
+print(f'A area é de {area(21, 1)}')
+print(f'A area é de {area(13, 8)}')
