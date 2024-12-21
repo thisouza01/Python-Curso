8085b2cb3d77451ad9150af63f4e4f68f3baf92c
unknown <tihsouza01@gmail.com>
2024-12-21 08:10:47 -0300

    termindo

---
+++ Funcoes/Ex100.py
@@ -0,0 +1,15 @@
+def fatorial(numero = 1, show = False):
+    # inicia resultado da soma
+    resultado = 1
+    
+    # fatorial
+    for i in range(numero, 0, -1):
+        resultado *= i      
+        # mostra o passo a passo, apenas se for verdade
+        if show == True:
+            print(f'{i}', end=' x ' if i > 1 else ' = ')
+
+
+
+fatorial(5)
+fatorial(5, True)
