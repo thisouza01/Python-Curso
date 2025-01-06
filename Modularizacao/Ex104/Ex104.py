514fa024a0ed46629abca8c86f2745ac64c120f5
unknown <tihsouza01@gmail.com>
2025-01-06 11:34:50 -0300

    organizando

--- Modularizacao/Ex104/Ex104.py
+++ Modularizacao/Ex104/Ex104.py
@@ -16,9 +16,6 @@ print(f'Um aumento de 10% no valor de {valor} é {moeda.aumento(valor)}')
 print(f'Uma diminuição de 13% no valor de {valor} é {moeda.reducao(valor)}')
 
 
-
-# aceitem um parametro a mais para saber se vai ser formatadp ou nao
-
 # resumo() -> moeda.resumo(p, 80, 35) -> preço, 80% aumento, 35 de redução
 # ========
 # resumo do valor
