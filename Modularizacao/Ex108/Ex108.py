a31559e008d64bf3d21e36d1a06ece5b02f80092
unknown <tihsouza01@gmail.com>
2025-01-06 16:23:57 -0300

    Exercicio 108

---
+++ Modularizacao/Ex108/Ex108.py
@@ -0,0 +1,12 @@
+import Utilidades.moeda.moeda as moeda
+# usar no programa principal
+valor = float(input('Valor: '))
+
+# resumo -> valor, aumento e redução
+moeda.resumo(valor, 20, 40)
+
+# metade
+print(f'A metade de {moeda.moeda(valor)} é {moeda.metade(valor, True)}')
+
+# dobro
+print(f'O dobro de {moeda.moeda(valor)} é {moeda.dobro(valor, True)}')
